d5947dd945104316963392e084ba00f3dc35e626
hassansaber <hassansaberkarin@gmail.com>
2022-08-17 13:01:34 +0430

    5 done

---
+++ logger.js
@@ -0,0 +1,13 @@
+// const text = "hassan";
+
+// const sayHi = (name) => {
+//   console.log(`hello ${name}`);
+// };
+
+// module.exports = { text, sayHi };
+
+// module.exports.greeting = sayHi;
+// module.exports.ts = text;
+
+// exports.greeting = sayHi;
+// module.exports.ts = text;
--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-const http = require("http");
+//4---------------------------------
+// const http = require("http");
 
-const server = http.createServer((req, res) => {
-  console.log(req);
-});
+// const server = http.createServer((req, res) => {
+//   console.log(req);
+// });
 
-server.listen(8000);
+// server.listen(8000);
+//5----------------------------------
+
+// const logger = require("./logger");
+// const { greeting } = require("./logger");
+
+// logger.sayHi("ali");
+// greeting("ali");
+
+const chalk = require("chalk");
+
+console.log(chalk.blue("Hello world!"));
