07b8c58bcb06f1d1f16f17d01284cbdd86ea4d92
hassansaber <hassansaberkarin@gmail.com>
2022-08-20 14:07:50 +0430

    10 done

--- main.js
+++ main.js
@@ -14,6 +14,27 @@
 // logger.sayHi("ali");
 // greeting("ali");
 
-const chalk = require("chalk");
+// const chalk = require("chalk");
 
-console.log(chalk.blue("Hello world!"));
+// console.log(chalk.blue("Hello world!"));
+
+//-----------------------------------------------------
+// console.log(process.title); // powershell or npm or..
+// console.log(process.pid); // 1324 ...
+// console.log(process.arch); //architecture x64
+// console.log(process.version); // node version
+// console.log(process.platform); //win 32
+// console.log(process.env); //environment variables like MOGO_URI
+// console.log(process.env.NODE_ENV); // future...(production/develop)
+// console.log(process.exit()); //close application code 0 to 12
+// //0 == while event loop is empty
+// //1 == while error
+// console.log(process.kill(type p id)); //kill process
+// process.on; //events
+// process.on('beforeExit',(code)=>console.log('exit code is'+ code)); // example of event
+// // events are async
+// //---
+// console.log(process.argv); // flags
+// console.log(process.cwd()); // location
+
+//---------------------------------------------------------------
