ec72773212a57ff63f84bd809e529ce9dfc0de04
hassansaber <hassansaberkarin@gmail.com>
2022-08-21 20:56:18 +0430

    11 done

--- main.js
+++ main.js
@@ -38,3 +38,48 @@
 // console.log(process.cwd()); // location
 
 //---------------------------------------------------------------
+
+const fs = require("fs");
+//fs  orginally is async
+
+fs.writeFile("test.txt", "helllo this is data", (err) => {
+  if (err) throw err;
+  console.log("data saved");
+});
+//file name , data , (add option), callback func(runs after everything)
+
+fs.appendFile("test.txt", " \n bye everyone", (err) => {
+  if (err) throw err;
+  console.log("data added");
+}); //append data to file
+
+// fs.writeFileSync().......
+// sync no need callback
+//and run line by line
+
+fs.rename("test.txt", "greeting.txt", (err) => {
+  if (err) throw err;
+  console.log("file name changed");
+});
+
+fs.unlink("greeting.txt", (err) => {
+  if (err) throw err;
+  console.log("file deleted");
+}); // delete file
+
+const os = require("os");
+
+console.log(os.platform()); //win 32
+console.log(os.arch()); // x64
+console.log(os.userInfo());
+//{
+//  uid: -1,
+//  gid: -1,
+//  username: 'Hassan',
+//  homedir: 'C:\\Users\\Hassan',
+//  shell: null
+//}
+
+console.log(os.release()); //10.0.4849
+console.log(os.hostname()); //DESKTOP-P4PKUPT
+console.log(os.uptime()); //32584 mil sec system is on
